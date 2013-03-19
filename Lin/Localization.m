95369456d17c31690ad38beb00c4683328022f95
Psi <psi.xie@gmail.com>
2013-03-19 17:51:40 +0800

    Support [ registration = "Registration"; ] style

--- Lin/Localization.m
+++ Lin/Localization.m
@@ -1,10 +1,10 @@
 /*
  Copyright (c) 2013 Katsuma Tanaka
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
@@ -22,18 +22,18 @@
 - (id)init
 {
     self = [super init];
-    
+
     if(self) {
         self.localizations = [NSMutableDictionary dictionary];
     }
-    
+
     return self;
 }
 
 - (void)dealloc
 {
     [_localizations release];
-    
+
     [super dealloc];
 }
 
@@ -49,37 +49,37 @@
 		filePathDictionary = [NSMutableDictionary dictionary];
 		[self.localizations setObject:filePathDictionary forKey:filePath];
 	}
-	
+
     // Detect language
     NSArray *pathComponents = [filePath pathComponents];
     NSString *directory = [pathComponents objectAtIndex:pathComponents.count - 2];
     NSString *language = [directory stringByDeletingPathExtension];
-    
+
     // Load
     NSString *contents = [NSString stringWithContentsOfFile:filePath encoding:encoding error:NULL];
-    
+
     // Parse
     NSMutableDictionary *localizationPairs = [NSMutableDictionary dictionary];
-    
+
     [contents enumerateLinesUsingBlock:^(NSString *line, BOOL *stop) {
         // Regular expression
-        NSRegularExpression *regularExpression = [NSRegularExpression regularExpressionWithPattern:@"\"(.*)\".*=.*\"(.*)\";$" options:0 error:NULL];
-        
+        NSRegularExpression *regularExpression = [NSRegularExpression regularExpressionWithPattern:@"\"?(.*)\"?\\s*=\\s*\"(.*)\";$" options:0 error:NULL];
+
         [regularExpression enumerateMatchesInString:line options:0 range:NSMakeRange(0, line.length) usingBlock:^(NSTextCheckingResult *result, NSMatchingFlags flags, BOOL *stop) {
             if(result.numberOfRanges == 3) {
                 NSRange keyRange = [result rangeAtIndex:1];
                 NSRange stringValueRange = [result rangeAtIndex:2];
-                
+
                 NSString *key = [line substringWithRange:keyRange];
                 NSString *stringValue = [line substringWithRange:stringValueRange];
-                
+
                 [localizationPairs setObject:stringValue forKey:key];
             }
-            
+
             *stop = YES;
         }];
     }];
-    
+
     [filePathDictionary setObject:[NSDictionary dictionaryWithDictionary:localizationPairs] forKey:language];
 }
 
@@ -91,7 +91,7 @@
     for(NSString *filePath in filePaths) {
         NSDictionary *languages = [self.localizations objectForKey:filePath];
         NSArray* pLanguageKeys = [languages allKeys];
-		
+
         for(NSString *key in pLanguageKeys) {
 			if (![languageItems objectForKey:key])
 				[languageItems setObject:[NSNull null] forKey:key];
@@ -114,7 +114,7 @@
 			[keys sortUsingComparator:^NSComparisonResult(NSString *key1, NSString *key2) {
             			return (NSComparisonResult)[key1 compare:key2];
        			}];
-       			
+
 			for(NSString *key in keys) {
 				NSString *stringValue = [localizationPairs objectForKey:key];
 
@@ -128,7 +128,7 @@
 			}
 		}
 	}];
-	
+
     return [NSArray arrayWithArray:localizationItems];
 }
 
