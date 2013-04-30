3a00079b32dd97f6ca719d9238cab881dc305779
Hubert SARRET <hubert.sarret@gmail.com>
2013-04-30 17:14:42 +0200

    Added Unit Testing target + some tests for Localization

---
+++ Tests/Tests.h
@@ -0,0 +1,13 @@
+//
+//  Tests.h
+//  Tests
+//
+//  Created by Hubert SARRET on 30/04/13.
+//  Copyright (c) 2013 Katsuma Tanaka. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Tests : SenTestCase
+
+@end
