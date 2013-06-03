108d1b01552ba8a0b5a17a86232d375a5bbf6c72
questbeat <questbeat@gmail.com>
2013-06-03 11:50:54 +0900

    Refactoring

---
+++ LinTests/LocalizationTests.h
@@ -0,0 +1,13 @@
+//
+//  LocalizationTests.h
+//  Lin
+//
+//  Created by Katsuma Tanaka on 2013/06/03.
+//  Copyright (c) 2013年 Katsuma Tanaka. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LocalizationTests : SenTestCase
+
+@end
